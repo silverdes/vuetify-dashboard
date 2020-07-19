a7b3d9b1e2851140088c03184b080fe23910229f
silverdev <ouaiji04@gmail.com>
2020-07-19 16:30:07 +0100

    Adding Side Drawer with routers

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer= !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase gery--text">
+        <span class="font-weight-light">Dash</span>
+        <span class="font-weight-medium">board</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      //list of routers
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
+    };
+  }
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
-
-export default new Vuetify({
+const vuetify = new Vuetify({
+    icons: {
+        iconfont: 'md',
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: '#393e46',
+                secondary: '#eeeeee',
+                anchor: '#8c9eff',
+            },
+        },
+    },
 });
+export default vuetify
